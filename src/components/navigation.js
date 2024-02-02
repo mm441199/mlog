612602f31323c067a0a3f4af004bca1f9f364dde
minamoto <mimoma987@gmail.com>
2024-02-02 22:32:15 +0900

    ブログカードのレスポンシブデザインを修正

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,27 +4,20 @@ import * as styles from '../styles/_navigation.module.scss'
 
 const Navigation = ({open}) => {
   return (
-    <nav className={styles.headerMenuNavigation}>
-    {/* <nav aria-expanded={open} className={styles.headerMenuNavigation}> */}
-      <ul className={styles.headerMenuNavigationList}>
-        <li className={styles.headerMenuNavigationListItem}>
-          <Link to ="" className={styles.headerMenuNavigationListItemLink}>HOME</Link>
+    <nav aria-expanded={open} className={styles.navigation}>
+      <ul className={styles.navigationList}>
+        <li className={styles.navigationListItem}>
+          <Link to ="" className={styles.navigationLink}>記事一覧</Link>
         </li>
-        <li className={styles.headerMenuNavigationListItem}>
-          <Link to ="/tags/shopify/" className={styles.headerMenuNavigationListItemLink}>shopify</Link>
+        <li className={styles.navigationListItem}>
+          <Link to ="/profile/" className={styles.navigationLink}>プロフィール</Link>
         </li>
-        <li className={styles.headerMenuNavigationListItem}>
-          <Link to ="/tags/liquid/" className={styles.headerMenuNavigationListItemLink}>liquid</Link>
-        </li>
-        <li className={styles.headerMenuNavigationListItem}>
-          <Link to ="/tags/gatsby/" className={styles.headerMenuNavigationListItemLink}>gatsbyJS</Link>
-        </li>
-        <li className={styles.headerMenuNavigationListItem}>
-          <Link to ="/form/" className={styles.headerMenuNavigationListItemLink}>CONTACT</Link>
+        <li className={styles.navigationListItem}>
+          <Link to ="/form/" className={styles.navigationLink}>お問い合わせ</Link>
         </li>
       </ul>
     </nav>
-  );
+  )
 };
 
 export default Navigation;
